3b76a7d60ceedba900159febc4d8ec013e681f17
Simon Kuenzer <simon.kuenzer@neclab.eu>
2015-03-02 08:33:14 +0100

    MiniCache: linuxapp: lwIP network drivers updated from lwip-contrib

--- target/linux/include/target/netdev.h
+++ target/linux/include/target/netdev.h
@@ -2,8 +2,13 @@
 #define _NETDEV_H_
 
 #include <netif/tapif.h>
+//#include <netif/tunif.h>
+//#include <netif/unixif.h>
 
 #define target_netif_init \
   tapif_init
+
+//tunif_init
+//unixif_init
 
 #endif
