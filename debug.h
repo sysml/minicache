9dad2537c90d15119fb265536e8c5de99b7d8be1
Simon Kuenzer <simon.kuenzer@neclab.eu>
2015-05-06 09:06:32 +0200

    Improved debug code

--- debug.h
+++ debug.h
@@ -12,33 +12,34 @@ extern struct timeval __debug_tsref;
 #ifdef __MINIOS__
 #define __debug_printf(fmt, ...) printk((fmt), ##__VA_ARGS__)
 #else
-#define __debug_printf(fmt, ...) printf((fmt), ##__VA_ARGS__)
+#define __debug_printf(fmt, ...) fprintf(stderr, (fmt), ##__VA_ARGS__)
 #endif /* __MINIOS__ */
 
 /**
  * printd(): prints a debug message to stdout
  */
-#define printd(fmt, ...)					\
-	do {							\
-	    struct timeval now;				\
-	    unsigned long mins, secs, usecs;			\
-	    							\
-	    gettimeofday(&now, NULL);				\
-	    if (now.tv_usec < __debug_tsref.tv_usec) {		\
-	      now.tv_usec += 1000000l;				\
-	      now.tv_sec--;					\
-	    }							\
-	    usecs = (now.tv_usec - __debug_tsref.tv_usec);	\
-	    							\
-	    secs  = (now.tv_sec - __debug_tsref.tv_sec);	\
-	    secs += usecs / 1000000l;				\
-	    usecs %= 1000000l;					\
-	    mins = secs / 60;					\
-	    secs %= 60;					\
-	    							\
-	    __debug_printf("[%lum%02lu.%06lus] ", mins, secs, usecs); \
-	    __debug_printf("%s:%4d: %s(): ", __FILE__, __LINE__, __FUNCTION__); \
-	    __debug_printf((fmt), ##__VA_ARGS__);		\
+#define printd(fmt, ...)						\
+	do {								\
+	    struct timeval now;					\
+	    uint64_t mins, secs, usecs;				\
+	    								\
+	    gettimeofday(&now, NULL);					\
+	    if (now.tv_usec < __debug_tsref.tv_usec) {			\
+	        now.tv_usec += 1000000l;				\
+	        now.tv_sec--;						\
+	    }								\
+	    usecs = (now.tv_usec - __debug_tsref.tv_usec);		\
+	    								\
+	    secs  = (now.tv_sec - __debug_tsref.tv_sec);		\
+	    secs += usecs / 1000000l;					\
+	    usecs %= 1000000l;						\
+	    mins = secs / 60;						\
+	    secs %= 60;						\
+	    								\
+	    __debug_printf("[%"PRIu64"m%02"PRIu64".%06"PRIu64"s] %s:%4d: %s(): " \
+			   fmt, mins, secs, usecs,			\
+			   __FILE__, __LINE__, __FUNCTION__,		\
+			   ##__VA_ARGS__);				\
 	} while(0)
 
 /**
