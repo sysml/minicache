e6d4258cec8eb14baf22cca6d4ae35472827c722
Simon Kuenzer <simon.kuenzer@neclab.eu>
2015-05-30 01:31:31 +0200

    MiniCache: Debug: Visualize calldepth on x86 MiniOS

--- debug.h
+++ debug.h
@@ -11,14 +11,93 @@ extern struct timeval __debug_tsref;
 
 #ifdef ENABLE_DEBUG
 #ifdef __MINIOS__
+#if (defined __x86_64__ || defined __x86_32__)
+#define CONFIG_DEBUG_CALLDEPTH
+#endif
 #define __debug_printf(fmt, ...) printk((fmt), ##__VA_ARGS__)
 #else
 #define __debug_printf(fmt, ...) fprintf(stderr, (fmt), ##__VA_ARGS__)
 #endif /* __MINIOS__ */
 
+#if defined __MINIOS__ && defined __x86_64__
+#define __get_bp() \
+	({ \
+		unsigned long bp; \
+		asm("movq %%rbp, %0":"=r"(bp)); \
+		bp; \
+	})
+#elif defined __MINIOS__ && defined __x86_32__
+#define __get_bp() \
+	({ \
+		unsigned long bp; \
+		asm("movq %%ebp, %0":"=r"(bp)); \
+		bp; \
+	})
+#endif
+
+#if defined __MINIOS__ && (defined __x86_64__ || defined __x86_32__)
+/**
+ * get_caller(): returns calling address for the current function
+ *
+ * Note: On non-x86 platforms, 0xBADC0DED is returned
+ */
+#define get_caller()	  \
+	({ \
+		unsigned long *frame = (void *) __get_bp(); \
+		frame[1]; \
+	})
+
+/**
+ * get_calldepth(): returns the current number of invoked function calls
+ *
+ * Note: On non-x86 platforms, 0x0 is returned always
+ */
+#define get_calldepth()	  \
+	({ \
+		unsigned long depth = 0;		      \
+		unsigned long *frame = (void *) __get_bp();   \
+		while (frame[0]) {			      \
+			++depth;			      \
+			frame = (void *) frame[0];	      \
+		}					      \
+		depth;					      \
+	})
+#else
+#define get_caller() (0xBADC0DED)
+#define get_calldepth() (0x0)
+#endif
+
 /**
  * printd(): prints a debug message to stdout
  */
+#ifdef CONFIG_DEBUG_CALLDEPTH
+#define printd(fmt, ...)						\
+	do {								\
+	    struct timeval now;					\
+	    uint64_t mins, secs, usecs;				\
+	    unsigned long cd = get_calldepth();			\
+	    								\
+	    gettimeofday(&now, NULL);					\
+	    if (now.tv_usec < __debug_tsref.tv_usec) {			\
+	        now.tv_usec += 1000000l;				\
+	        now.tv_sec--;						\
+	    }								\
+	    usecs = (now.tv_usec - __debug_tsref.tv_usec);		\
+	    								\
+	    secs  = (now.tv_sec - __debug_tsref.tv_sec);		\
+	    secs += usecs / 1000000l;					\
+	    usecs %= 1000000l;						\
+	    mins = secs / 60;						\
+	    secs %= 60;						\
+	    								\
+	    __debug_printf("[%"PRIu64"m%02"PRIu64".%06"PRIu64"s] ",	\
+			     mins, secs, usecs);			\
+	    while (cd--)						\
+		__debug_printf("-");					\
+	    __debug_printf(" %s():%d: "				\
+			   fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+	} while(0)
+#else
 #define printd(fmt, ...)						\
 	do {								\
 	    struct timeval now;					\
@@ -42,38 +121,13 @@ extern struct timeval __debug_tsref;
 			   __FILE__, __LINE__, __FUNCTION__,		\
 			   ##__VA_ARGS__);				\
 	} while(0)
-
-/**
- * get_caller(): returns calling address for the current function
- *
- * Note: On non-x86 platforms, 0xBADC0DED is returned
- */
-#if defined __MINIOS__ && defined __x86_64__
-#define get_caller()	  \
-	({ \
-		unsigned long bp; \
-		unsigned long *frame; \
-		asm("movq %%rbp, %0":"=r"(bp)); \
-		frame = (void*) bp; \
-		frame[1]; \
-	})
-#elif defined __MINIOS__ && defined __x86_32__
-#define get_caller()	  \
-	({ \
-		unsigned long bp; \
-		unsigned long *frame; \
-		asm("movl %%ebp, %0":"=r"(bp)); \
-		frame = (void*) bp; \
-		frame[1]; \
-	})
-#else
-#define get_caller() 0xBADC0DED
 #endif
 
 #else /* ENABLE_DEBUG */
 
 #define printd(fmt, ...) do {} while(0)
-#define get_caller() 0xDEADC0DE
+#define get_caller() (0xDEADC0DE)
+#define get_calldepth() (0x0)
 
 #endif /* ENABLE_DEBUG */
 #endif /* _DEBUG_H_ */
