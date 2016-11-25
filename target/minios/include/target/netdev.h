5eecb316c8b068d47e2a4095f7ab0e5c46863e6c
Simon Kuenzer <simon.kuenzer@neclab.eu>
2016-11-25 16:10:43 +0100

    MiniCache on Mini-OS: Support for netmapfront

--- target/minios/include/target/netdev.h
+++ target/minios/include/target/netdev.h
@@ -3,6 +3,22 @@
 
 #include <mini-os/lwip-net.h>
 
+#ifdef CONFIG_NETMAPFRONT
+#include <mini-os/netmapfront.h>
+
+#define target_netif_init \
+  netmapfrontif_init
+#define target_netif_poll \
+  netmapfrontif_poll
+
+#ifdef CONFIG_SELECT_POLL
+#define CAN_POLL_NETDEV
+#define target_netif_fd \
+  netmapfrontif_fd
+#endif /* CONFIG_SELECT_POLL */
+
+#else
+
 #define target_netif_init \
   netfrontif_init
 #define target_netif_poll \
@@ -14,4 +30,5 @@
   netfrontif_fd
 #endif /* CONFIG_SELECT_POLL */
 
+#endif /* CONFIG_NETMAPFRONT */
 #endif /* _NETDEV_H_ */
